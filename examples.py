467032ef4e79822d761cc82f8da128fd9fa411ab
Elena Popova <drraggygd@inbox.ru>
2023-12-14 11:16:13 +0300

    initial commit

---
+++ examples.py
@@ -0,0 +1,12 @@
+print(list(range(3, 15, 2)))
+#            start stop step
+
+print(list(range(2, 7)))  # range(2, 7, 1) -> 2 3 4 5 6 
+#            start stop 
+
+print(list(range(6)))  # range(0, 6, 1) -> 0 1 2 3 4 5
+#              stop
+
+print(list(range(5, 0, -1)))  # 5 4 3 2 1
+#            start stop step
+
